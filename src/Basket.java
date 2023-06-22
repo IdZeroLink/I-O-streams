846e1b50b755381d63bb6dc1fb1ce85a0a153390
IdZeroLink <valertosboxer@yandex.ru>
2023-06-22 21:41:30 +0300

    last commit

--- src/Basket.java
+++ src/Basket.java
@@ -1,15 +1,24 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class Basket {
-    protected String[] products;
-    protected int[] prices;
-    protected int[] basket = new int[5];
-    protected File file = new File("basket.bin");
+public class Basket implements Serializable {
+    private static final long serialVersionUID = 1L;
+    protected String products[];
+    protected int prices[];
+    protected int basket[];
 
     public Basket() {
-        this.products = new String[]{"Мандарины- ", "Яблоки- ", "Груши- ", "Хлеб- ", "Молоко- "};
+    }
+
+
+    public Basket (String[] products, int[] prices) {
+        this.products = new String[]{"Мандарины ", "Яблоки ", "Груши ", "Хлеб ", "Молоко "};
         this.prices = new int[]{100, 150, 120, 50, 130};
+        this.basket = new int[this.products.length];
     }
 
     public Basket(String[] products, int[] prices, int[] basket) {
@@ -20,47 +29,106 @@ public class Basket {
 
     public void addToCart(int productNum, int amount) {
         basket[productNum - 1] += amount;
+
     }
 
     public void printCart() {
-        int sumProducts = 0;
-        System.out.println("Ваша корзина: ");
-        System.out.println();
+        int sumPurchase = 0;
         for (int i = 0; i < basket.length; i++) {
-            if (basket[i] <= 0) {
-                continue;
+            if (basket[i] > 0) {
+                int pricePurchase = prices[i] * basket[i];
+                sumPurchase += pricePurchase;
+                System.out.println(products[i] + "-" + basket[i] + "шт" + " на сумму: " + pricePurchase
+                );
             }
-            int priceProduct = basket[i] * prices[i];
-            sumProducts += priceProduct;
-            System.out.println(products[i] + ": " + basket[i] + "шт " + prices[i] + " шт/рублей." + " Итого: " + priceProduct + " рублей.");
         }
-        System.out.println("Сумма корзины: " + sumProducts + " рублей.");
+        System.out.println("Сумма вашей корзины : " + sumPurchase);
     }
 
-    public File getFile() {
-        return file;
+    public void saveTxt(File textFile) throws FileNotFoundException {
+        try (PrintWriter writer = new PrintWriter(textFile)) {
+            for (String product : products) {
+                writer.print(product + " ");
+            }
+            writer.print("\n");
+            for (int prices : prices) {
+                writer.print(prices + " ");
+            }
+            writer.print("\n");
+            for (int i : basket) {
+                writer.print(i + " ");
+            }
+        }
     }
-
-    public void saveBin(File file) throws IOException {
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file))) {
-            out.writeObject(new Basket(products, prices, basket));
+    public static Basket loadFromTxtFile(File textFile) throws FileNotFoundException {
+        Basket basket = new Basket();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(textFile))) {
+            String productsStr = bufferedReader.readLine();
+            String pricesStr = bufferedReader.readLine();
+            String basketStr = bufferedReader.readLine();
+            basket.products = productsStr.split(" ");
+            basket.prices = Arrays.stream(pricesStr.split(" "))
+                    .map(Integer::parseInt)
+                    .mapToInt(Integer::intValue)
+                    .toArray();
+            basket.basket = Arrays.stream(basketStr.split(" "))
+                    .map(Integer::parseInt)
+                    .mapToInt(Integer::intValue)
+                    .toArray();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return basket;
+    }
+    public void saveBin(File file) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
+            oos.writeObject(this);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public static Basket loadFromBinFile(File file) throws Exception {
+    public static Basket loadFromBinFile(File file) {
         Basket basket = null;
-        try (ObjectInputStream ins = new ObjectInputStream(new FileInputStream(file))) {
-            basket = (Basket) ins.readObject();
-            return basket;
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+            basket = (Basket) ois.readObject();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return basket;
+    }
+    public void saveJSON(File file)  {
+        try (PrintWriter writer= new PrintWriter(file)) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String json =gson.toJson(this);
+            writer.print(json);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    @Override
-    public String toString() {
-        System.out.println("Список товаров для покупки: ");
-        for (int i = 0; i < products.length; i++) {
-            System.out.println(" Товар: " + " " + products[i] + prices[i] + " рублей.");
+    public static Basket loadFromJSONFile(File file){
+        Basket basket ;
+        try (BufferedReader reader= new BufferedReader(new FileReader(file))) {
+            StringBuilder builder = new StringBuilder();
+            String line =null;
+            while ((line=reader.readLine()) != null) {
+                builder.append(line);
+            }
+            Gson gson = new Gson();
+            basket = gson.fromJson(builder.toString(), Basket.class);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        return " ";
+        return basket;
     }
 }
+
+
